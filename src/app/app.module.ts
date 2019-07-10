e8cc80b4ec1a2e973cc00d7bd14c1353fbcdd2cc
Pagbo <plmrt1@gmail.com>
2019-07-10 11:37:38 +0100

    Pony Racer project initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PonyComponent } from './pony/pony.component';
+import { RacesComponent } from './races/races.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PonyComponent,
+    RacesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pony/pony.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pony',
+  templateUrl: './pony.component.html',
+  styleUrls: ['./pony.component.scss']
+})
+export class PonyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
